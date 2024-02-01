c1ad58a5b076a39adddecc36d48c3e6f9948ad1c
Marco <marcoolivera096@gmail.com>
2024-02-01 16:15:58 -0300

    subindo arquivos corrigidos

--- aulas/aula389/aula389.py
+++ aulas/aula389/aula389.py
@@ -4,34 +4,40 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3' 
 DB_FILE =ROOT_DIR/DB_NAME
-TABLE_NAME = 'customers'
+TABLE_NAME = 'costumers'
 
 connection =sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
-# cuidado: fazendo delete   sem where
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# CUIDADO: fazendo delete sem where
 cursor.execute(
     f'DELETE FROM {TABLE_NAME}'
 )
-
 cursor.execute(
-    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}" '
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
 )
+connection.commit()
+
+# Cria a tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
-    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-    'name TEXT,'
-    'weight REAL'
-    ')'
+
 )
 connection.commit()
-#registrar valores na coluna da tabela
+
+# Registrar valores nas colunas da tabela
 # CUIDADO: sql injection
 cursor.execute(
-    f'INSERT INTO {TABLE_NAME} (id, name, weight)'
+    f'INSERT INTO {TABLE_NAME} '
+    '(id, name, weight) '
     'VALUES '
-    '(NULL,  "Helena", 6.7), (NULL , "Eduardo", 10)'
-    )
+    '(NULL, "Helena", 4), (NULL, "Eduardo", 10)'
+)
 connection.commit()
+
 cursor.close()
 connection.close()
