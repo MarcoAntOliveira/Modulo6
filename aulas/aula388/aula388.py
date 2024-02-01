cc86695754768b09ad64461c0a77c725e1f48f3f
Marco <marcoolivera096@gmail.com>
2024-02-01 09:41:56 -0300

    second commit

---
+++ aulas/aula388/aula388.py
@@ -0,0 +1,28 @@
+import sqlite3 
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3' 
+DB_FILE =ROOT_DIR/DB_NAME
+TABLE_NAME = 'costumers'
+
+connection =sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight)'
+    'VALUES '
+    '(NULL,  "Helena", 6.7), (NULL , "Eduardo", 10)'
+    )
+
+cursor.close()
+connection.close()
